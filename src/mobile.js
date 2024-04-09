9e00603dbbf7c453e2220a8c21846312234c9a3b
petrudemian <petredemi@gmail.com>
2024-04-09 19:44:18 +0100

    modifiy .gitignore

--- src/mobile.js
+++
@@ -1,7 +0,0 @@
-const dropbtn = document.querySelectorAll('div.dropdown > button');
-const myDropDown = document.querySelector('#myDropdown');
-const dropdownContent = document.querySelector('div.dropdown > div.dropdown-content');
-dropbtn.addEventListener('click', (e) => {
-          dropbtn.style.backgroundColor = 'red';
-          dropdownContent.setAttribute('style', 'display: block');
-});
